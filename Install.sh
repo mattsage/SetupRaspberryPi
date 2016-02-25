bae36295a70a7c689f3f24a60b903238005ef60b
mattsage <mattsage@users.noreply.github.com>
2016-02-25 11:55:41 +0000

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+############################
+#TO DO
+############################
+#edit wifi connections
+#Copy my scripts across
+#edit rc.local to run IP Script
+#Raspberry Pi Camera
+#VLC
+#Unified remote
+#Static IP
+#YouTube Workshop Scripts
+#Transmission
+
+#Change Password
+echo "############################"
+echo "Please enter a new password"
+echo "############################"
+passwd
+
+#Update and Upgrade
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo dist-upgrade -y
+
+#Pibrella
+sudo apt-get install -y python-pip
+sudo pip install -y pibrella
+
+#Sense Hat
+sudo apt-get install -y sense-hat
+sudo pip-3.2 install -y pillow
+cp /usr/src/sense-hat/examples ~/ -a
+
+#Unicorn Hat
+curl -sS get.pimoroni.com/unicornhat | bash
+
+#Display-o-Tron
+curl get.pimoroni.com/dot3k | bash
+
+#GPIO Zero
+sudo apt-get install -y python3-gpiozero
+
+#Arduino
+sudo apt-get install -y arduino
+
+#GParted
+sudo apt-get install -y gparted
+
+#Autopsy and Sleuthkit
+sudo apt-get install -y sleuthkit
+sudo apt-get install -y autopsy
+
+#Pushbullet
+pip install git+https://github.com/Azelphur/pyPushBullet.git
+
+#Uptimed (Uprecords)
+sudo apt-get install -y uptimed
+
+#Codebug
+sudo apt-get install -y python3-codebug-i2c-tether
+
+#DOTs Board
+sudo pip install rpi_dots_minecraft
+
+#Fail2Ban
+sudo apt-get install fail2ban
+
+#Blynk
+sudo apt-get install build-essential
+sudo npm install -g npm
+sudo npm install -g onoff
+sudo npm install -g blynk-library
+
+#End script
+sudo raspi-config
